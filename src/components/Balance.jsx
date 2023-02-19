667886b91d88bfd5bbbf49b963347290f9cb184a
Fernando Ponce <fernando.p11@epn.edu.ec>
2023-02-18 21:30:45 -0500

    Panatalla Registar Balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,31 +1,30 @@
 import {useEffect, useState} from "react"
 import {useNavigate} from "react-router-dom"
 
-
 function Balance({ balance }) {
-    const { ID_INFORME_FINANCIERO, ACTIVOS, PASIVOS, PATRIMONIO } = balance
-    const [date, setDate] = useState(new Date().toLocaleDateString());
-    const navigate = useNavigate()
+const { ID_INFORME_FINANCIERO, ACTIVOS, PASIVOS, PATRIMONIO } = balance
+const [date, setDate] = useState(new Date().toLocaleDateString());
+const navigate = useNavigate()
+return (
+    <tr className="border-b">
+        <td className="text-center">
+            {ID_INFORME_FINANCIERO}
+        </td>
+        <td className="text-center">{date}</td>
+        <td className="text-center">
+            <div className="p-4 flex justify-center gap-3">
+                <button
+                    type="button"
+                    className="text-blue-600 hover:text-green-700 uppercase font-bold text-xs"
+                    onClick={() => navigate(`#`)}
+                >
+                    Ver Detalle
+                </button>
+            </div>
+        </td>
+    </tr>
+);
 
-    return (
-        <tr className="border-b">
-            <td className="text-center">
-                {ID_INFORME_FINANCIERO}
-            </td>
-            <td className="text-center">{date}</td>
-            <td className="text-center">
-                <td className="p-4 flex justify-center gap-3">
-                    <button
-                        type="button"
-                        className="text-blue-600 hover:text-green-700 uppercase font-bold text-xs"
-                        onClick={() => navigate(`#`)}
-                    >
-                        Ver Detalle
-                    </button>
-                </td>
-            </td>
-        </tr>
-    );
 }
 
 export default Balance
--- src/components/FormularioBalance.jsx
+++ src/components/FormularioBalance.jsx
@@ -1,96 +1,27 @@
-import { Link, useLoaderData } from "react-router-dom";
-import {useState, useForm} from "react"
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-const FormularioBalance = (informe_financiero_balance) => {
 
-    
+const FormularioBalance = () => {
+    const [date, setDate] = useState(new Date().toISOString().substr(0, 10));
+    const { register, handleSubmit, errors } = useForm();
+  
+    const handleChange = (event) => {
+      setDate(event.target.value);
+    };
+  
+    const onSubmit = (data) => {
+      console.log(data);
+    };
   
     return (
-        <>
-            <div className="my-4">
-                <p>Fecha Inicio: {date}</p>
-                <input
-                    type="date"
-                    onChange={handleChange}
-                    ref={dateInputRef}
-                    id="FECHA_INICIO"
-                    name="FECHA_INICIO"
-                />
-            </div>  
+      <>
 
-            <div className="my-4">
-                <p>Fecha Fin: {date}</p>
-                <input
-                    type="date"
-                    onChange={handleChange}
-                    ref={dateInputRef}
-                    id="FECHA_FIN"
-                    name="FECHA_FIN"
-                />
-            </div>     
+       
+  
+      </>
+    );
+  };
+export default FormularioBalance;
 
-
-
-            <div className="mb-4">
-                <label
-                    className="flex justify-start text-gray-800"
-                    htmlFor="patrimonio_empleado"
-                >Patrimonio:</label>
-                <input 
-                    id="patrimonio_empleado"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-black"
-                    placeholder="patrimonio del empleado"
-                    name="patrimonio_empleado"
-                />
-            </div>
-            <div className="mb-4">
-                <label
-                    className=" flex justify-start text-gray-800"
-                    htmlFor="activos_balance"
-                >Activos:</label>
-                <input 
-                    id="activos_balance"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-black"
-                    placeholder="Apellido del empleado"
-                    name="activos_balance"
-                />
-            </div>
-            <div className="mb-4">
-                <label
-                    className=" flex justify-start text-gray-800"
-                    htmlFor="pasivos_balance"
-                >Pasivos:</label>
-                <input 
-                    id="pasivos_balance"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-black"
-                    placeholder="Cedula del empleado"
-                    name="pasivos_balance"
-                    //pattern="[0-9]{10}"
-                />
-            </div>
-            <div className="mb-4">
-                <label
-                    className=" flex justify-start text-gray-800"
-                    htmlFor="resultado"
-                >Resultado:</label>
-                <input 
-                    id="resultado"
-                    type="text"
-                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-black"
-                    placeholder="E-mail del empleado"
-                    name="resultado"
-                    //pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$"
-                />
-            </div>
-          
-            
-        </>
-    )
-}
-
-export default FormularioBalance
-
-
+  
