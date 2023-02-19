77a13486a10103c942fa22d7da585a366e588cbd
Bezno <Bezno@users.noreply.github.com>
2023-02-19 18:15:22 -0500

    //fix - bucle detalle pedidos

--- src/components/FormularioDetalle_Pedido.jsx
+++ src/components/FormularioDetalle_Pedido.jsx
@@ -4,10 +4,7 @@ import React, {useEffect, useRef, useState} from "react";
 const FormularioDetalle_Pedido = ({detalle_pedido, pedido, items}) => {    
     
     const [item, setItems] = useState("")
-    console.log(item)
-    const [precio, setPrecio] = useState("")
-    const [cantidad, setCantidad] = useState(1)
-    const [captureItem, setCaptureItem] = useState([
+    const [detalle, setDetalle] = useState([
         {
             id_pedido: '',
             id_item: '',
@@ -15,86 +12,27 @@ const FormularioDetalle_Pedido = ({detalle_pedido, pedido, items}) => {
             precio_detalle_pedido: ''
         }
     ])
-    
+    const insertarItem = (numPedido,item,cantidad,precio) =>{
+        const nextItem = [
+            // Items before the insertion point:
+            ...detalle.slice(0, 0),
+            // New item:
+            { id_pedido: numPedido, id_item: item, cantidad_pedido:cantidad, precio_detalle_pedido: precio},
+            // Items after the insertion point:
+            ...detalle.slice(0)
+          ];
+          setDetalle(nextItem);
+          console.log(detalle);
+    }
+
+    const [cantidad, setCantidad] = useState('')
+
     return (
-        // <div className="grid grid-cols-3 gap-4">
-        //     <div className="my-4"> Pedido 
-        //     {
-        //         <input
-        //         id="id_pedido"
-        //         name="id_pedido"
-        //         type="text"
-        //         className="block w-full p-3 bg-gray-50"
-        //         value={pedido.ID_PEDIDO}
-        //         readOnly
-        //         />
-        //     }
-        //     </div>
-            
-        //     <div className="my-4">
-        //     {
-                    
-        //     }
-        //     </div>
-        //     <div className="my-4">
-        //     {
-                    
-        //     }
-        //     </div>
-            
-        //     <div className="my-4"> Seleccione un item
-        //     {items.length ?(
-        //             <select 
-        //                 id="id_item" 
-        //                 name="id_item" 
-        //                 onChange={(event)=>setItems(event.target.value)} 
-        //                 className="block w-full p-3 bg-gray-50"
-        //                 >
-        //                 <option >Selecciona un item</option>
-        //                 {items.map(item =>(
-        //                     <option key={item.ID_ITEM} value={item.ID_ITEM}>{item.NOMBRE_ITEM}</option>
-        //                 ))}
-        //             </select>
-                    
-        //             ):(<p> No existen Items</p>)}
-        //     </div>
-
-        //     <div className="my-4">Cantidad
-        //     {
-        //         <input
-        //         id="cantidad_pedido"
-        //         name="cantidad_pedido"
-        //         type="number"
-        //         className="block w-full p-3 bg-gray-50"
-        //         placeholder={1}
-        //         />
-        //     }
-        //     </div>
-
-        //     <div className="my-4"> Precio unitario
-        //     {items.length ?(
-        //             <select 
-        //                 id="precio_detalle_pedido" 
-        //                 name="precio_detalle_pedido" 
-        //                 onChange={(event)=>setItems(event.target.value)} 
-        //                 className="block w-full p-3 bg-gray-50"
-        //                 >
-        //                 {items.map(item =>(
-        //                     <option key={item.ID_ITEM} value={item.ID_ITEM}>{item.PRECIO_ITEM}</option>
-        //                 ))}
-        //             </select>
-                    
-        //             ):(<p> No existen Items</p>)}
-        //     </div>
-            
-        // </div>
-
-
 
     /////// NUEVO FORMULARIO ////////
 
         <div className="grid grid-cols gap-4">
-            {captureItem.map((input, index) => {
+            {detalle.map((input, index) => {
           return (
             <div key={index}>
               <div className="my-4"> Pedido 
@@ -131,7 +69,9 @@ const FormularioDetalle_Pedido = ({detalle_pedido, pedido, items}) => {
                                     placeholder={cantidad}
                                     defaultValue={1}
                                     onChange={(event)=>setCantidad(event.target.value)}
-                                    />
+                                    
+                                    />{console.log(cantidad)}
+                                <button onClick={()=>insertarItem(pedido.ID_PEDIDO, item.ID_ITEM, cantidad, item.PRECIO_ITEM)}>Añadir producto</button>
                             </div>
                             </div>
                          ))}
