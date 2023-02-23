41dbe259745bb9d55d47642c3cfef78b6e63c7bd
Bezno <Bezno@users.noreply.github.com>
2023-02-23 00:45:41 -0500

    /update - pruebas envío de arrays detalle

--- src/components/FormularioDetalle_Pedido.jsx
+++ src/components/FormularioDetalle_Pedido.jsx
@@ -42,7 +42,7 @@ const FormularioDetalle_Pedido = ({detalle_pedido, pedido, items}) => {
                       <div 
                           id='id_item' 
                           name='id_item'
-                          className="grid grid-cols-3"
+                          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
                           >
                           {items.map(item =>(
                              <ItemVenta
--- src/components/ItemVenta.jsx
+++ src/components/ItemVenta.jsx
@@ -1,24 +1,15 @@
 import {useState} from 'react'
 
 function ItemVenta({item, pedido, obtenerDetalle}) {
-    const [cantidad, setCantidad] = useState(1)
-    const [detalle, setDetalle] = useState([
-        {
-            id_pedido: '',
-            id_item: '',
-            cantidad_pedido: '',
-            precio_detalle_pedido: ''
-        }
-    ])
+    const [cantidad, setCantidad] = useState(0)
+    const [detalle, setDetalle] = useState([])
 
     const insertarItem = (numPedido,item,cantidad,precio) =>{
-        const nextItem = [
-                    // Items before the insertion point:
-                    ...detalle.slice(0, 0),
-                    // New item:
-                    { id_pedido: numPedido, id_item: item, cantidad_pedido:cantidad, precio_detalle_pedido: precio}
-                  ];
-        setDetalle(prevDetalle => [...prevDetalle, nextItem])
+        // const nextItem = [
+        //             // New item:
+        //             { id_pedido: numPedido, id_item: item, cantidad_pedido:cantidad, precio_detalle_pedido: precio}
+        //           ];
+        setDetalle(prevDetalle => [...prevDetalle, { id_pedido: numPedido, id_item: item, cantidad_pedido:cantidad, precio_detalle_pedido: precio}])
         console.log(detalle)
     }
 
@@ -34,21 +25,26 @@ function ItemVenta({item, pedido, obtenerDetalle}) {
 
     return (
         <>
-            <div key={ID_ITEM} value={ID_ITEM}
+            <div
+                key={ID_ITEM}
+                value={ID_ITEM}
+                id="id_item"
+                name="id_item"
                 className="grid-cols-3 block p-3 m-3 bg-gray-50 text-center rounded overflow-hidden shadow-lg"
             >
                 <div>
                     <div>{NOMBRE_ITEM}</div>
-                    <div id="precio_detalle_pedido" className="precio_detalle_pedido">${PRECIO_ITEM * cantidad}</div>
+                    <div id="precio_detalle_pedido" name="precio_detalle_pedido" className='text-left ml-5'>Precio: <div className='text-center'>${PRECIO_ITEM}</div></div>
                     <input
                         id="cantidad_pedido"
                         name="cantidad_pedido"
                         type="number"
-                        className="block w-full p-3 bg-gray-50"
+                        className="block w-full p-3 bg-gray-50 text-center"
                         // placeholder={cantidad}
-                        defaultValue={1}
+                        defaultValue={0}
                         onChange={(event) => setCantidad(event.target.value)}
                     />
+                    <div>${PRECIO_ITEM * cantidad}</div>
                     <div onClick={() => insertarItem(ID_PEDIDO, ID_ITEM, cantidad, PRECIO_ITEM)}>Añadir producto</div>
                 </div>
             </div>
