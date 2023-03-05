742f56c7004a7e203d1814a0a1d64ea85c7e576a
Danrylus <danrylus@gmail.com>
2023-03-05 18:59:56 -0500

    Agregados íconos

--- src/pages/EmpleadosMenu.jsx
+++ src/pages/EmpleadosMenu.jsx
@@ -7,51 +7,42 @@ function EmpleadosMenu() {
     const location = useLocation()
 
     return (
-
         <>
             <h1 className="font-black text-4xl text-black">Empleados</h1>
             <p className="mt-3">Selecciona las acciones a realizar con los empleados</p>
 
-            <div className=" flex justify-start bg-black text-white rounded md: w-3/4 mx-auto px-5 py-2 mt-6">Acciones Pedidos</div>
-            <div className="bg-white shadow rounded-md md: w-3/4 mx-auto px-5 py-10 mt-5">
+            <div className="flex justify-start bg-black text-white rounded w-3/4 mx-auto px-5 py-2 mt-6">Acciones Empleados</div>
+            <div className="bg-white shadow rounded-md w-3/4 mx-auto px-5 py-10 mt-5">
                 <div className="grid grid-cols-2 gap-10">
-                    <button
-                        className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                        <Link
-                            state={location.state}
-                            to='/empleados'>Empleados</Link>
-                    </button>
-                    <button
-                        className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                        <Link
-                            state={location.state}
-                            to='/empleados/iess'>Parámetros del IESS</Link>
-                    </button>
-                    <button
-                        className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                        <Link
-                            state={location.state}
-                            to='/empleados/departamentos'>Departamentos</Link>
-                    </button>
-                    <button
-                        className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                        <Link
-                            state={location.state}
-                            to='/empleados/cargos'>Cargos</Link>
-                    </button>
+                    <div className="flex flex-col items-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/554/554744.png" alt="Icono de empleados" className="h-16 w-16 object-contain mb-2" />
+                        <button className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                            <Link state={location.state} to='/empleados'>Empleados</Link>
+                        </button>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/8382/8382909.png" alt="Icono de parámetros del IESS" className="h-16 w-16 object-contain mb-2" />
+                        <button className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                            <Link state={location.state} to='/empleados/iess'>Parámetros del IESS</Link>
+                        </button>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/4343/4343053.png" alt="Icono de departamentos" className="h-16 w-16 object-contain mb-2" />
+                        <button className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                            <Link state={location.state} to='/empleados/departamentos'>Departamentos</Link>
+                        </button>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/6713/6713388.png" alt="Icono de cargos" className="h-16 w-16 object-contain mb-2" />
+                        <button className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                            <Link state={location.state} to='/empleados/cargos'>Cargos</Link>
+                        </button>
+                    </div>
                 </div>
-
             </div>
-
         </>
+    );
 
-
-
-    )
 }
 
 export default EmpleadosMenu
--- src/pages/Finanzas.jsx
+++ src/pages/Finanzas.jsx
@@ -4,65 +4,49 @@ import { Link, useLocation } from "react-router-dom"
 function Finanzas() {
 
     const location = useLocation()
-
     return (
-
         <>
             <h1 className="font-black text-4xl text-black">Finanzas</h1>
             <p className="mt-3">Selecciona las acciones a realizar con las finanzas</p>
 
-            <div className=" flex justify-start bg-black text-white rounded md: w-3/4 mx-auto px-5 py-2 mt-6">Acciones Pedidos</div>
-            <div className="bg-white shadow rounded-md md: w-3/4 mx-auto px-5 py-10 mt-5">
+            <div className="flex justify-start bg-black text-white rounded md:w-3/4 mx-auto px-5 py-2 mt-6">Acciones de Finanzas</div>
+
+            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-5">
                 <div className="grid grid-cols-2 gap-10">
-                    <button
-                        className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                        <Link
-                            state={location.state}
-                            to='/finanzas/cuentas'>Cuentas</Link>
-                    </button>
-                    <button
-                        className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                        <Link
-                            state={location.state}
-                            to='/finanzas/asientos'>Asientos</Link>
-                    </button>
-                    <button
-                        className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                        <Link
-                            state={location.state}
-                            to='/finanzas/bancos'>Bancos</Link>
-                    </button>
-                    <button
-                        className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                        <Link
-                            state={location.state}
-                            to='/finanzas/balance'>Balance General</Link>
-                    </button>
 
-                    {/*}
-                <button
-                    className="flex justify-center mt-3 ml-5 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm"
-                    >
-                <Link
+                    <div className="flex flex-col items-center justify-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/8870/8870291.png" alt="Cuentas" className="w-24 h-24" />
+                        <button className="flex justify-center mt-3 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                            <Link state={location.state} to='/finanzas/cuentas'>Cuentas</Link>
+                        </button>
+                    </div>
 
-                    to='/finanzas/estado'>Estado de Resultados</Link>
-                </button>
+                    <div className="flex flex-col items-center justify-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/1354/1354873.png" alt="Asientos" className="w-24 h-24" />
+                        <button className="flex justify-center mt-3 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                            <Link state={location.state} to='/finanzas/asientos'>Asientos</Link>
+                        </button>
+                    </div>
 
-                */}
+                    <div className="flex flex-col items-center justify-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/2543/2543409.png" alt="Bancos" className="w-24 h-24" />
+                        <button className="flex justify-center mt-3 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                            <Link state={location.state} to='/finanzas/bancos'>Bancos</Link>
+                        </button>
+                    </div>
 
+                    <div className="flex flex-col items-center justify-center">
+                        <img src="https://cdn-icons-png.flaticon.com/512/6926/6926233.png" alt="Balance" className="w-24 h-24" />
+                        <button className="flex justify-center mt-3 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                            <Link state={location.state} to='/finanzas/balance'>Balance General</Link>
+                        </button>
+                    </div>
 
                 </div>
-
             </div>
         </>
+    );
 
-
-
-    )
 }
 
 export default Finanzas
