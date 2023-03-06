0d1ba218979d6eac16064f753a1e084fe563bb36
JennyL00 <55715205+JennyL00@users.noreply.github.com>
2023-03-06 16:06:15 -0500

    listo-05-03

--- src/components/Cargo_empleado.jsx
+++ src/components/Cargo_empleado.jsx
@@ -24,11 +24,6 @@ function Cargo({ cargo }) {
                     onClick={() => navigate(`/empleados/cargos/${ID_CARGO_EMPLEADO}/editar`)}
                 >Editar
                 </button>
-                <button type="submit"
-                    className="text-red-600 hover:text-blue-700 uppercase font-bold text-xs"
-                    onClick={() => navigate(`/empleados/cargos/${ID_CARGO_EMPLEADO}/eliminar`)}>
-                    Eliminar
-                </button>
             </td>
         </tr>
     )
