460c512a7ea5f3e790631efe61329fdc0f3b1eb8
Christian Fernandez <christian.fernandez@epn.edu.ec>
2023-03-12 21:57:51 -0500

    Create - vista Item-Receta

---
+++ src/components/ItemReceta.jsx
@@ -0,0 +1,85 @@
+import { Form, useNavigate, redirect } from "react-router-dom"
+import { eliminarItem } from "../data/items"
+
+export async function action({params}){
+    await eliminarItem(params.itemId)
+    
+    return redirect('/produccion')
+}
+
+function ItemReceta({ item}) {
+
+    const navigate = useNavigate()
+    const { ID_ITEM, ID_LISTA_ITEMS, ID_ESTADO_PRODUCION,ESTADO_PRODUCCION, ID_TIPO_ITEM, TIPO_ITEM, CODIGO_ITEM, NOMBRE_ITEM, FECHA_FABRI_ITEM, FECHA1_CADU_ITEM, FECHA2_CADU_ITEM,
+        LOTE_ITEM, UNIDAD_MEDIDA,CANTIDAD_LOTE_ITEM, PRECIO_ITEM, PESO_ITEM, CONSERVACION_ITEM, DETALLE_ITEM, ID_RECETAP} = item
+
+    return (
+        <tr className="">
+            <td align="center">
+                {ID_LISTA_ITEMS}
+            </td>
+            <td>
+                {ESTADO_PRODUCCION}
+            </td>
+            <td>
+                {TIPO_ITEM}
+            </td>
+            <td>
+                {CODIGO_ITEM}
+            </td>
+            <td>
+                {NOMBRE_ITEM}
+            </td>
+            <td>
+                {FECHA_FABRI_ITEM}
+            </td>
+            <td>
+                {FECHA1_CADU_ITEM}
+            </td>
+            <td>
+                {FECHA2_CADU_ITEM}
+            </td>
+            <td>
+                {LOTE_ITEM}
+            </td>
+            <td align="center">
+                {CANTIDAD_LOTE_ITEM}
+            </td>
+            <td>
+                {UNIDAD_MEDIDA}
+            </td>
+            <td align="center">
+                {PESO_ITEM}
+            </td>
+            <td align="center">
+                {PRECIO_ITEM}
+            </td>
+            <td>
+                {CONSERVACION_ITEM}
+            </td>
+            <td>
+                {DETALLE_ITEM}
+            </td>
+            <td className="p-4 flex justify-center gap-3">
+                <button type="button"
+                        className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs" 
+                        onClick={() => navigate(`/item/${ID_ITEM}/actualizar`)}>Editar</button>
+                <Form
+                    method='POST'
+                    action={`/item/${ID_ITEM}/eliminar`}
+                    onSubmit={ (e) => {
+                        if (!confirm('¿Deseas eliminar este registro?')){
+                            e.preventDefault()
+                        }
+                    }}
+                >
+                <button type="submit"
+                        className="text-red-600 hover:text-blue-700 uppercase font-bold text-xs"
+                    >Eliminar</button>
+                </Form>
+            </td>
+        </tr>
+    )
+}
+
+export default ItemReceta
---
+++ src/pages/ListaItemReceta.jsx
@@ -0,0 +1,66 @@
+import { Link, useLoaderData } from "react-router-dom";
+import { obtenerItems } from "../data/items";
+import ItemReceta from "../components/ItemReceta";
+import { cuentasInventario } from "../data/cuentas.js"
+
+export async function loader() {
+    await cuentasInventario(0)
+
+    const items = obtenerItems()
+    return items
+    
+}
+
+function ItemsReceta() {
+
+    const items = useLoaderData()
+
+    return (
+        <>
+            <h1 className="font-black text-4xl">Producto</h1>
+            <p>Estacion de creacion de producctos</p>
+
+            <div className="rounded-md md: w-11/12 mx-auto px-5 py-10 mt-5">
+                {items.length ? (
+                    <table className="w-full bg-white shadow mt-5 table-auto">
+                        <thead className="bg-black text-white">
+                            <tr>
+                                <th className="p-2">Id Lista Item</th>
+                                <th className="p-2">Estado Produccion</th>
+                                <th className="p-2">Tipo Item</th>
+                                <th className="p-2">Codigo Item</th>
+                                <th className="p-2">Nombre Item</th>
+                                <th className="p-2">Fecha Fabricación</th>
+                                <th className="p-2">Fecha Caducidad 1</th>
+                                <th className="p-2">Fecha Caducidad 2</th>
+                                <th className="p-2">Lote Item</th>
+                                <th className="p-2">Cantidad Lote</th>
+                                <th className="p-2">Unidad Medida</th>
+                                <th className="p-2">Peso Item</th>
+                                <th className="p-2">Precio Item</th>
+                                <th className="p-2">Conservación Item</th>
+                                <th className="p-2">Detalle Item</th>
+                                <th className="p-2">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {items.map( item => (
+                                    <ItemReceta 
+                                        item={item}
+                                        key={item.ID_ITEM}
+                                    />
+                                    ))}
+                        </tbody>
+                    </table>
+                ): (<p className="text-center mt-10"> No existe Item</p> )}
+
+                {/*<button className="mt-3 rounded bg-orange-300 p-2 uppercase font-bold text-black text-sm">
+                    <Link
+                        to='/item/nuevo'>CREAR</Link>
+                </button>*/}
+            </div>
+        </>
+    )
+}
+
+export default ItemsReceta
