a244d27f128902ed9b910ffb6d72eb6de56abb94
Bezno <carlos_wilfrido666@hotmail.com>
2023-03-13 22:14:21 -0500

    //fix - factura

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -58,24 +58,8 @@ function Pedido({ pedido }) {
   }
 
   function handleEliminar(){
-    if(ESTADO_PEDIDO=="Entregado"){
-      return(true)
-    }else{
-      return(false)
-    }
+    return ESTADO_PEDIDO=="Entregado";
   }
-
-  // const invoice = {
-  //   id: '123456',
-  //   date: '2023-03-10',
-  //   items: [
-  //     { id: '1', name: 'Item 1', price: 10.0 },
-  //     { id: '2', name: 'Item 2', price: 20.0 },
-  //   ],
-  //   subtotal: 30.0,
-  //   taxes: 3.0,
-  //   total: 33.0,
-  // };
 
   return (
     <>
@@ -149,23 +133,20 @@ function Pedido({ pedido }) {
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            {/* <div className="relative w-auto my-6 mx-auto max-w-3xl"> */}
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
+                {/* <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t"> */}
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
+                      x
                     </span>
                   </button>
-                </div>
+                {/* </div> */}
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <Invoice pedido={pedido}  />
@@ -177,20 +158,19 @@ function Pedido({ pedido }) {
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Close
+                    Cerrar
                   </button>
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Save Changes
+                    Aceptar
                   </button>
                 </div>
               </div>
-            </div>
+            {/* </div> */}
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
 
